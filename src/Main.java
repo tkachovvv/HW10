9749b6b399b1cdb8002502368207a8d2d223ec69
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-11-17 00:10:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String phone = "+995-593-763-817";
+        phone = phone.replace("-", "");
+        phone = phone.replace(" ", "");
+        phone = phone.replace("+", "");
+        if (phone.length()==9) {
+            phone = "+995" + phone;
+        } else if (phone.length() > 13) {
+            throw new RuntimeException("Телефон слишком длинный!");
+        } else if (phone.length() < 9) {
+            throw new RuntimeException("Телефон слишком короткий!");
+        }else if (phone.length()== 10 && phone.charAt(0) != '+'){
+            throw new RuntimeException("Среди нас посторонний!");
+        }
+        System.out.println("phone = " + phone);
+        String expectedPhone = "593763817";
+        if (phone.equals(expectedPhone)) {
+            System.out.println("Успех!");
+        } else {
+            System.out.println("Неудача!");
+        }
+
+    }
+}
